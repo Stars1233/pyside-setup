Stars1233/pyside-setup

commit 09e0acbad03601158a252a4c5d7f15585d6b52ce

limited api: Remove PyCFunction_GET_FUNCTION, PyCFunction_GET_SELF, and PyCFunction_GET_FLAGS
Removing old compatibility macros from the initial limited api
implementation.

Change-Id: I3044609ade36b5b378de05934eab8a098450f42f
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

sources/shiboken6/libshiboken/pep384impl.h
@@ -274,9 +274,6 @@ enum PepUnicode_Kind {
 #ifdef Py_LIMITED_API
 
 using PyCFunctionObject = struct _pycfunc;
-#define PyCFunction_GET_FUNCTION(func)  PyCFunction_GetFunction((PyObject *)func)
-#define PyCFunction_GET_SELF(func)      PyCFunction_GetSelf((PyObject *)func)
-#define PyCFunction_GET_FLAGS(func)     PyCFunction_GetFlags((PyObject *)func)
 #define PepCFunction_GET_NAMESTR(func) \
     _PepUnicode_AsString(PyObject_GetAttrString((PyObject *)func, "__name__"))
 #else

sources/pyside6/libpyside/pysidesignal.cpp
@@ -764,7 +764,7 @@ static PyObject *signalCall(PyObject *self, PyObject *args, PyObject *kw)
     Shiboken::AutoDecRef homonymousMethod(PepExt_Type_CallDescrGet(signal->homonymousMethod,
                                                                    nullptr, nullptr));
     if (PyCFunction_Check(homonymousMethod.object())
-            && (PyCFunction_GET_FLAGS(homonymousMethod.object()) & METH_STATIC))
+            && (PyCFunction_GetFlags(homonymousMethod.object()) & METH_STATIC))
         return PyObject_Call(homonymousMethod, args, kw);
 
     // Assumes homonymousMethod is not a static method.
@@ -1248,10 +1248,9 @@ QByteArray getCallbackSignature(QMetaMethod signal, QObject *receiver,
         }
 #endif
     } else if (PyCFunction_Check(callback)) {
-        const auto *funcObj = reinterpret_cast<const PyCFunctionObject *>(callback);
-        functionName = PepCFunction_GET_NAMESTR(funcObj);
-        useSelf = PyCFunction_GET_SELF(funcObj) != nullptr ? 1 : 0;
-        const int flags = PyCFunction_GET_FLAGS(funcObj);
+        functionName = PepCFunction_GET_NAMESTR(callback);
+        useSelf = PyCFunction_GetSelf(callback) != nullptr ? 1 : 0;
+        const int flags = PyCFunction_GetFlags(callback);
 
         if (receiver) {
             // Search for signature on metaobject

sources/pyside6/libpyside/qobjectconnect.cpp
@@ -111,7 +111,7 @@ static GetReceiverResult getReceiver(QMetaMethod signal, PyObject *callback)
         result.receiver = PySide::convertToQObject(result.self, false);
 #endif
     } else if (PyCFunction_Check(callback)) {
-        result.self = PyCFunction_GET_SELF(callback);
+        result.self = PyCFunction_GetSelf(callback);
         result.receiver = PySide::convertToQObject(result.self, false);
     } else if (PySide::isCompiledMethod(callback)) {
         result.self = PyObject_GetAttr(callback, Shiboken::PyName::im_self());

sources/shiboken6/libshiboken/signature/signature.cpp
@@ -173,7 +173,7 @@ PyObject *GetSignature_Function(PyObject *obfunc, PyObject *modifier)
     if (props == nullptr)
         Py_RETURN_NONE;
 
-    int flags = PyCFunction_GET_FLAGS(obfunc);
+    int flags = PyCFunction_GetFlags(obfunc);
     PyObject *func_kind;
     if (PyModule_Check(obtype_mod.object()))
         func_kind = PyName::function();

sources/shiboken6/libshiboken/signature/signature_helper.cpp
@@ -239,7 +239,7 @@ PyObject *_get_class_of_bm(PyObject *ob_bm)
 
 PyObject *_get_class_of_cf(PyObject *ob_cf)
 {
-    PyObject *selftype = PyCFunction_GET_SELF(ob_cf);
+    PyObject *selftype = PyCFunction_GetSelf(ob_cf);
     if (selftype == nullptr) {
         selftype = PyDict_GetItem(pyside_globals->map_dict, ob_cf);
         if (selftype == nullptr) {

commit 2f1a1b49d6f135c12b8efe9f4ced9f2291a8482a

Fix build for non-limited API
PepCFunction_GET_NAMESTR previously expected a PyCFunctionObject*,
which was broken by 09e0acbad03601158a252a4c5d7f15585d6b52ce.
Fix the function to take a PyObject *, which seems more in-line
with the other PyCFunction functions.

Change-Id: I464451e42054c19126c11d4ba04b127c0a772bc3
Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

sources/shiboken6/libshiboken/pep384impl.h
@@ -277,7 +277,8 @@ using PyCFunctionObject = struct _pycfunc;
 #define PepCFunction_GET_NAMESTR(func) \
     _PepUnicode_AsString(PyObject_GetAttrString((PyObject *)func, "__name__"))
 #else
-#define PepCFunction_GET_NAMESTR(func)        ((func)->m_ml->ml_name)
+#define PepCFunction_GET_NAMESTR(func) \
+    (reinterpret_cast<const PyCFunctionObject *>(func)->m_ml->ml_name)
 #endif
 
 /*****************************************************************************