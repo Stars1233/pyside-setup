ba41ec2ec76c6af84a1ac42ff02f2fd518ab0db3
Simo Fält <simo.falt@qt.io>
2025-02-17 14:11:46 +0200

    Install libclang for Windows arm config
    There is only static libs provisioned on VM and it seems that we can't
    build shiboken6 with those.
    
    Change-Id: I04fd633405cda0c11efab5d1f2cbd532a0f2b3c9
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

---
+++ coin/fetch_libclang_arm64.ps1
@@ -0,0 +1,8 @@
+# Copyright (C) 2024 The Qt Company Ltd.
+# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+# Download the file
+
+wget https://download.qt.io/development_releases/prebuilt/libclang/libclang-release_19.1.0-based-windows-vs2022_arm64.7z -o libclang.7z
+# Unzip the contents to /home/qt
+7z x libclang.7z -o/utils
+Remove-Item libclang.7z
