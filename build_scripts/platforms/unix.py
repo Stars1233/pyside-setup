Stars1233/pyside-setup

commit 38c0d909ff78f5432d4a99a55974fda35786f868

Fix multimedia to work on Linux with 6.8.1
The helper libraries are now also required on Linux. Also,
some new libraries libQt6FFmpegStub*.so* were added.

Pick-to: 6.8
Change-Id: I78ecd66139b18f48bbbe677990dc8ec570238199
Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>
Reviewed-by: Shyamnath Premnadh <Shyamnath.Premnadh@qt.io>

build_scripts/platforms/unix.py
@@ -12,7 +12,8 @@ from ..utils import copydir, copyfile, copy_qt_metatypes, makefile
 from .. import PYSIDE, SHIBOKEN
 from .linux import prepare_standalone_package_linux
 from .macos import prepare_standalone_package_macos
-from .. import PYSIDE_UNIX_BIN_TOOLS, PYSIDE_UNIX_LIBEXEC_TOOLS, PYSIDE_UNIX_BUNDLED_TOOLS
+from .. import (PYSIDE_UNIX_BIN_TOOLS, PYSIDE_UNIX_LIBEXEC_TOOLS, PYSIDE_UNIX_BUNDLED_TOOLS,
+                PYSIDE_MULTIMEDIA_LIBS)
 
 
 def _macos_copy_gui_executable(name, _vars=None):
@@ -233,11 +234,12 @@ def prepare_packages_posix(pyside_build, _vars, cross_build=False):
                 "{st_build_dir}/{st_package_name}/jar",
                 _vars=_vars)
 
-    # some libraries specific to Android from 6.8
+    # Some libraries specific to Linux/Android from 6.8
     # eg: the libav* libraries are required for the multimedia module
-    if is_android and config.is_internal_pyside_build():
+    if config.is_internal_pyside_build() and sys.platform != "darwin":
+        qt_multimedia_filters = [f"lib{lib}*.so*" for lib in PYSIDE_MULTIMEDIA_LIBS]
         copydir("{qt_lib_dir}", destination_qt_dir / "lib",
-                _filter=["libav*.so", "libsw*.so"],
+                _filter=qt_multimedia_filters,
                 recursive=False, _vars=_vars, force_copy_symlinks=True)
 
     # Copy Qt libs to package

build_scripts/__init__.py
@@ -39,3 +39,6 @@ PYSIDE_UNIX_LIBEXEC_TOOLS = ["uic",
 
 # all Qt tools are in 'bin' folder in Windows
 PYSIDE_WINDOWS_BIN_TOOLS = PYSIDE_UNIX_LIBEXEC_TOOLS + PYSIDE_LINUX_BIN_TOOLS
+
+PYSIDE_MULTIMEDIA_LIBS = ["avcodec", "avformat", "avutil",
+                          "swresample", "swscale"]

build_scripts/platforms/windows_desktop.py
@@ -13,7 +13,7 @@ from ..config import config
 from ..options import OPTION
 from ..utils import (copydir, copyfile, copy_qt_metatypes,
                      download_and_extract_7z, filter_match, makefile, in_coin)
-from .. import PYSIDE, SHIBOKEN, PYSIDE_WINDOWS_BIN_TOOLS
+from .. import PYSIDE, SHIBOKEN, PYSIDE_WINDOWS_BIN_TOOLS, PYSIDE_MULTIMEDIA_LIBS
 
 
 def prepare_packages_win32(pyside_build, _vars):
@@ -282,13 +282,7 @@ def copy_qt_artifacts(pyside_build, destination_qt_dir, copy_pdbs, _vars):
         copy_qt_permanent_artifacts = False
 
     # <qt>/bin/*.dll and Qt *.exe -> <setup>/{st_package_name}
-    qt_multimedia_artifacts_permanent = [
-        "avcodec-*.dll",
-        "avformat-*.dll",
-        "avutil-*.dll",
-        "swresample-*.dll",
-        "swscale-*.dll"
-    ]
+    qt_multimedia_artifacts_permanent = [f"{lib}-*.dll" for lib in PYSIDE_MULTIMEDIA_LIBS]
 
     def qt_rhi_artifacts_permanent():
         result = ["opengl*.dll"]

build_scripts/wheel_files.py
@@ -10,6 +10,7 @@ from dataclasses import Field, dataclass, field
 
 _pyside_package_path = None
 _module_json_file_path = None
+_multimedia_libs = ["avcodec", "avformat", "avutil", "swresample", "swscale"]
 
 
 def set_pyside_package_path(p):
@@ -878,15 +879,12 @@ def module_QtMultimedia() -> ModuleData:
     data.translations.append("qtmultimedia_*")
     data.plugins = get_module_plugins(json_data)
 
+    linux_multimedia_libs = [f"Qt/lib/lib{lib}*.so*" for lib in _multimedia_libs]
+    linux_multimedia_libs.append("Qt/lib/libQt6FFmpegStub*.so*")
     platform_files = {
-        "win32": ["avcodec-*.dll", "avformat-*.dll", "avutil-*.dll", "swresample-*.dll",
-                  "swscale-*.dll"],
-        "darwin": [f"Qt/lib/{dependency_lib}" for dependency_lib in ["libavcodec.*.dylib",
-                                                                     "libavformat.*.dylib",
-                                                                     "libavutil.*.dylib",
-                                                                     "libswresample.*.dylib",
-                                                                     "libswscale.*.dylib"]]}
-
+        "win32": [f"{lib}-*.dll" for lib in _multimedia_libs],
+        "darwin": [f"Qt/lib/lib{lib}.*.dylib" for lib in _multimedia_libs],
+        "linux": linux_multimedia_libs}
     extra_files = platform_files.get(sys.platform, [])
     data.extra_files.extend(extra_files)
 

commit 82ed44824050117a2197dcf20478a9fa79e34ad3

Android: Fix missing multimedia libs
- Regression introduced by 38c0d909ff78f5432d4a99a55974fda35786f868

Pick-to: 6.8 6.9
Change-Id: I172514816a1768c1de4df115c450b0d52afc63dd
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@qt.io>

build_scripts/platforms/unix.py
@@ -248,7 +248,7 @@ def prepare_packages_posix(pyside_build, _vars, cross_build=False):
 
     # Some libraries specific to Linux/Android from 6.8
     # eg: the libav* libraries are required for the multimedia module
-    if config.is_internal_pyside_build() and sys.platform != "darwin":
+    if config.is_internal_pyside_build() and (sys.platform != "darwin" or is_android):
         qt_multimedia_filters = [f"lib{lib}*.so*" for lib in PYSIDE_MULTIMEDIA_LIBS]
         copydir("{qt_lib_dir}", destination_qt_dir / "lib",
                 _filter=qt_multimedia_filters,