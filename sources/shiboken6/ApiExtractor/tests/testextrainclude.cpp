3cf2077a1b060bbea3419ccde23c5da6485a2e24
Friedemann Kleint <Friedemann.Kleint@qt.io>
2025-11-14 10:31:50 +0100

    Extend target triplet test by aarch64-poky-linux
    Task-number: PYSIDE-3235
    Task-number: PYSIDE-3105
    Change-Id: If2a30172bdb5544d221e5cb713ec9336600c18cc
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

--- sources/shiboken6/ApiExtractor/tests/testextrainclude.cpp
+++ sources/shiboken6/ApiExtractor/tests/testextrainclude.cpp
@@ -84,9 +84,15 @@ void TestExtraInclude::testParseTriplet_data()
 
     QTest::newRow("Linux")
         << QString("x86_64-unknown-linux-gnu"_L1)
-        << true << Architecture::X64 << Platform::Linux << true << Compiler::Gpp
+        << true << Architecture::X64 << Platform::Linux << false << Compiler::Gpp
         << false << QVersionNumber{}
         << "x86_64-unknown-linux-gnu"_ba;
+
+    QTest::newRow("Poky Linux")
+        << QString("aarch64-poky-linux"_L1)
+        << true << Architecture::Arm64<< Platform::Linux << false << Compiler::Gpp
+        << false << QVersionNumber{}
+        << "arm64-unknown-linux"_ba;
 
     QTest::newRow("WindowsArm")
         << QString("aarch64-pc-windows-msvc19.39.0"_L1)
