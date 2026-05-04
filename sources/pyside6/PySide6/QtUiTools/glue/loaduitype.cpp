d1b8c365655a5f304a0c9f8a88ed40f4578bbc11
Friedemann Kleint <Friedemann.Kleint@qt.io>
2026-05-04 13:53:39 +0200

    PySide6/QtUiTools: Move loadUiType() into static module sources
    The code needs security markers, so it should be a separate
    file. Making it a proper C++ source also eases code scanning.
    
    Extract a helper function to parse the className/baseClassName
    from XML in order to perform checks later on.
    
    Streamline the code a bit and use string literals.
    Fix the message to use native separators and double quotes.
    
    Complements 2cfeebb1f9fae34ba6ec52e76408dfa4acc0fbd2.
    
    Task-number: PYSIDE-3328
    Task-number: PYSIDE-1223
    Change-Id: If4318b69a15c5d47513d11b51601b19f1433b328
    Reviewed-by: Cristian Maureira-Fredes <cristian.maureira-fredes@qt.io>

---
+++ sources/pyside6/PySide6/QtUiTools/glue/loaduitype.cpp
@@ -0,0 +1,194 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "loaduitype.h"
+
+#include <pysideutils.h>
+
+#include <QtCore/qdir.h>
+#include <QtCore/qfile.h>
+#include <QtCore/qfileinfo.h>
+#include <QtCore/qoperatingsystemversion.h>
+#include <QtCore/qprocess.h>
+#include <QtCore/qxmlstream.h>
+
+#include <autodecref.h>
+
+#include <optional>
+
+QT_BEGIN_NAMESPACE
+
+using namespace Qt::StringLiterals;
+
+static QString getUicBinary()
+{
+    QString binary = u"pyside6-uic"_s;
+    if constexpr (QOperatingSystemVersion::currentType() == QOperatingSystemVersion::Windows)
+        binary += ".exe"_L1;
+
+    QString result = PySide::sysExecutable();
+    if (auto pos = result.lastIndexOf(u'/'); pos != -1) {
+        ++pos;
+        result.replace(pos, result.size() - pos, binary);
+        if (!QFileInfo::exists(result))
+            result = binary;
+    } else {
+        result = binary;
+    }
+    return result;
+}
+
+// Problem: The generated Python file doesn't have the Qt Base class information.
+// Solution: Use the XML file.
+
+struct XmlClassNames
+{
+    QString className;
+    QString baseClassName;
+};
+
+static std::optional<XmlClassNames> getXmlClassNames(const QString &uiFileName)
+{
+    QFile uiFile(uiFileName);
+    if (!uiFile.open(QIODevice::ReadOnly)) {
+        qCritical().noquote().nospace()
+            << "loadUiType: Cannot open " << QDir::toNativeSeparators(uiFileName)
+            << ": " << uiFile.errorString();
+        return std::nullopt;
+    }
+
+    // This will look for the first <widget> tag, e.g.:
+    //      <widget class="QWidget" name="ThemeWidgetForm">
+    // and then extract the information from "class", and "name",
+    // to get the baseClassName and className respectively
+    QString className;
+    QString baseClassName;
+    QXmlStreamReader reader(&uiFile);
+    while (!reader.atEnd() && baseClassName.isEmpty() && className.isEmpty()) {
+        auto token = reader.readNext();
+        if (token == QXmlStreamReader::StartElement && reader.name() == u"widget") {
+            baseClassName = reader.attributes().value("class").toString();
+            className = reader.attributes().value("name").toString();
+        }
+    }
+
+    if (reader.hasError()) {
+        qCritical().noquote().nospace()
+            << "loadUiType: An error occurred when parsing the UI file "
+               "while looking for the class info: " << reader.errorString();
+        return std::nullopt;
+    }
+
+    if (baseClassName.isEmpty() || className.isEmpty()) {
+        qCritical("loadUiType: The class name elements could not be found.");
+        return std::nullopt;
+    }
+
+    return XmlClassNames{className, baseClassName};
+}
+
+PyObject *loadUiType(PyObject *obFileName)
+{
+    // 1. Generate the Python code from the UI file
+    const QString uiFileName = PySide::pyUnicodeToQString(obFileName);
+    if (uiFileName.isEmpty()) {
+        qCritical("loadUiType: Error converting the UI filename");
+        Py_RETURN_NONE;
+    }
+
+    if (!QFileInfo::exists(uiFileName)) {
+        qCritical().noquote() << "loadUiType: File" << QDir::toNativeSeparators(uiFileName)
+                              << "does not exist";
+        Py_RETURN_NONE;
+    }
+
+    static const QString uicBin = getUicBinary();
+    if (!uicBin.contains(u'/')) {
+        qWarning("loadUiType(): \"%s\" could not be found in the Python installation, "
+                 "falling back to using a relative path. This poses a security risk. "
+                 "Please contact the application vendor to fix the issue.", qPrintable(uicBin));
+    }
+
+    QProcess uicProcess;
+    uicProcess.start(uicBin, {uiFileName});
+    if (!uicProcess.waitForStarted()) {
+        qCritical().noquote().nospace()
+            << "loadUiType(): Cannot run \"" << QDir::toNativeSeparators(uicBin)
+            << "\": " << uicProcess.errorString() << " - Check if 'pyside6-uic' is in PATH";
+        Py_RETURN_NONE;
+    }
+
+    if (!uicProcess.waitForFinished()
+        || uicProcess.exitStatus() != QProcess::NormalExit
+        || uicProcess.exitCode() != 0) {
+        qCritical().noquote().nospace() << '"' << QDir::toNativeSeparators(uicBin) << "\" failed: "
+            << uicProcess.errorString() << " - Exit status " << uicProcess.exitStatus()
+            << " (" << uicProcess.exitCode() << ")\n";
+        Py_RETURN_NONE;
+    }
+
+    const QByteArray uiFileContent = uicProcess.readAllStandardOutput();
+    const QByteArray errorOutput = uicProcess.readAllStandardError();
+
+    if (!errorOutput.isEmpty()) {
+        qCritical().noquote().nospace() << "loadUiType: \"" << QDir::toNativeSeparators(uicBin)
+                                        << "\" failed: " << errorOutput;
+        Py_RETURN_NONE;
+    }
+
+    // 2. Obtain the 'classname' and the Qt base class.
+    auto classNamesOpt = getXmlClassNames(uiFileName);
+    if (!classNamesOpt.has_value())
+        Py_RETURN_NONE;
+
+    QByteArray pyClassName("Ui_" + classNamesOpt->className.toUtf8());
+
+    PyObject *module = PyImport_ImportModule("__main__");
+    PyObject *loc = PyModule_GetDict(module);
+
+    // 3. exec() the code so the class exists in the context: exec(uiFileContent)
+    // The context of PyRun_SimpleString is __main__.
+    // 'Py_file_input' is the equivalent to using exec(), since it will execute
+    // the code, without returning anything.
+    Shiboken::AutoDecRef codeUi(Py_CompileString(uiFileContent.constData(), "<stdin>",
+                                                 Py_file_input));
+    if (codeUi.isNull()) {
+        qCritical("loadUiType: Error while compiling the generated Python file");
+        Py_RETURN_NONE;
+    }
+    PyObject *uiObj = PyEval_EvalCode(codeUi, loc, loc);
+
+    if (uiObj == nullptr) {
+        qCritical("loadUiType: Error while running exec() on the generated code");
+        Py_RETURN_NONE;
+    }
+
+    // 4. eval() the name of the class on a variable to return
+    // 'Py_eval_input' is the equivalent to using eval(), since it will just
+    // evaluate an expression.
+    Shiboken::AutoDecRef codeClass(Py_CompileString(pyClassName.constData(), "<stdin>",
+                                                    Py_eval_input));
+    if (codeClass.isNull()) {
+        qCritical() << "Error while compiling the Python class";
+        Py_RETURN_NONE;
+    }
+
+    Shiboken::AutoDecRef codeBaseClass(Py_CompileString(classNamesOpt->baseClassName.toUtf8().constData(),
+                                                        "<stdin>", Py_eval_input));
+    if (codeBaseClass.isNull()) {
+        qCritical("loadUiType: Error while compiling the base class");
+        Py_RETURN_NONE;
+    }
+
+    PyObject *classObj = PyEval_EvalCode(codeClass, loc, loc);
+    PyObject *baseClassObj = PyEval_EvalCode(codeBaseClass, loc, loc);
+
+    PyObject *result = PyTuple_Pack(2, classObj, baseClassObj);
+    if (result == nullptr) {
+        qCritical("loadUiType: Error while creating the return Tuple");
+        Py_RETURN_NONE;
+    }
+    return result;
+}
+
+QT_END_NAMESPACE
---
+++ sources/pyside6/PySide6/QtUiTools/glue/loaduitype.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2026 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#ifndef LOADUITYPE_H
+#define LOADUITYPE_H
+
+#include <sbkpython.h>
+
+#include <QtCore/qtclasshelpermacros.h>
+
+QT_BEGIN_NAMESPACE
+
+PyObject *loadUiType(PyObject *obFileName);
+
+QT_END_NAMESPACE
+
+#endif // LOADUITYPE_H
